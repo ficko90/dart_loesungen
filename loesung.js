b1c627538671f176605d53113e4410da77d177b1
F.Marin <f.marin@bcr.schule>
2021-06-08 12:07:33 +0200

    FINALE loesung entworfen

---
+++ loesung.js
@@ -0,0 +1,23 @@
+
+
+function findNextInt(y)
+{
+	var arr = [ 225, 256, 232, 1036, 532, 224, 10006 ];
+	var result = -1;
+	for(var i = 0; i < arr.length; i++) //durch kompletten Array laufen
+	{
+
+        var element = arr[i];
+
+		if(element > y && (element < result || result == -1) ) //falls y kleiner als arr[i] sein soll, muss auch geprüft werden ob auch result größer als arr[i] ist
+		{
+			result = element;
+		}
+	}
+	return result;
+}
+
+	console.log(findNextInt(226) );
+	console.log(findNextInt(250) );
+	console.log(findNextInt(2000) );
+	console.log(findNextInt(10007) );
