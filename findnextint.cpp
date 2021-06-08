da925bed3fd4308441d17a8519a9c106ec50848d
F.Marin <f.marin@bcr.schule>
2021-06-08 12:06:37 +0200

    dritte loesung entworfen

--- findnextint.cpp
+++ findnextint.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int Max(int array[], int n)
-{
-	int i;
 
-    // Loop to store largest number to arr[0]
-    for(i = 1;i < n; ++i)
-    {
-       // Change < to > if you want to find the smallest element
-       if(array[0] < array[i])
-           array[0] = array[i];
-    }
-
-    return array[0];
-}
-
-int FindNextInt(int y)
+int findNextInt(int y)
 {
 	int arr[7] = { 225, 256, 232, 1036, 532, 224, 10006 };
 	int length = sizeof(arr) / sizeof(arr[0]);
-	int max = Max(arr, length);
-	max++;
-	int result = max;
+	int result = 0;
+    bool foundResult = 0;
 
 	for(int i = 0; i < length; i++) //durch kompletten Array laufen
 	{
+        if(i==0){
+			result = arr[i];
+        }
 		if(arr[i] > y && result > arr[i]) //falls y kleiner als arr[i] sein soll, muss auch geprüft werden ob auch result größer als arr[i] ist
 		{
 			result = arr[i];
+            foundResult = 1;
 		}
 	}
+	cout << result << endl;
 
-	if(result == max)
-	{
-		result = 0;
-	}
-	
-	return result;
+	return (foundResult) ? result : -1;
 }
 
 int main()
 {
-	cout << FindNextInt(226) << endl;
-	cout << FindNextInt(250) << endl;
-	cout << FindNextInt(2000) << endl;
-	cout << FindNextInt(1000) << endl;
+	cout << findNextInt(226) << endl;
+	cout << findNextInt(250) << endl;
+	cout << findNextInt(2000) << endl;
+	cout << findNextInt(1000) << endl;
 }
